951c29a7661ce6f91aacebf2134882c98c62b42d
Abdel <abdel@exascale.info>
2019-02-28 12:52:39 +0100

    FIX | loading scripts

--- loadingScripts/loadScripts.py
+++ loadingScripts/loadScripts.py
@@ -1,6 +1,3 @@
-import pandas as pd
-
-
 # Loading csv datasets
 def load_csv():
     dataset_df = pd.read_csv("text_emotion.csv")
@@ -11,7 +8,6 @@ def load_csv():
 from xml.dom import minidom
 import nltk
 import collections
-
 
 
 def _preprocessing(X):
@@ -52,6 +48,7 @@ def _SemEval2007_Path2String(X_path, y_path):
 
     file = open(y_path)
     lines = file.read().split('\n')
+    # print(len(lines))
     lines = lines[0:-1]
 
     assert len(itemlist) == len(lines), 'data size not uniform'
@@ -73,6 +70,9 @@ def _SemEval2007_Path2String(X_path, y_path):
     return X, y
 
 
+import pandas as pd
+
+
 def Load_SemEval2007Data():
     DATA_X_PATH1 = "AffectiveText.Semeval.2007/AffectiveText.test/affectivetext_test.xml"
     DATA_Y_PATH1 = "AffectiveText.Semeval.2007/AffectiveText.test/affectivetext_test.emotions.gold"
@@ -83,50 +83,66 @@ def Load_SemEval2007Data():
     X1, y1 = _SemEval2007_Path2String(DATA_X_PATH1, DATA_Y_PATH1)
     X2, y2 = _SemEval2007_Path2String(DATA_X_PATH2, DATA_Y_PATH2)
     # y.update(y2)
-    print(X1)
-    print(y1)
+    # print(X1)
+    # print(y1)
 
-    df= pd.DataFrame([X1, y1])
+    df = pd.DataFrame([X1, y1])
 
-    for column in df:         # retenir le score maximal
-        df[column][1]= max(df[column][1])
-        print(df[column])
-        
-    return df
+    emotions = ["anger", "disgust", "fear", "joy", "sadness", "surprise"]
+    for column in df:  # retenir le score maximal
+        df[column][1] = emotions[df[column][1].index(max(df[column][1]))]
+        # print(df[column])
+
+    return df.T
 
 
+# Load_SemEval2007Data().to_pickle("SemEval.bin")
+# print(pd.read_pickle("SemEval.bin"))
 
 # Loading arff dataset
 
-import arff
+from scipy.io import arff
 import pandas as pd
 
-def loadArff:
-df = pd.DataFrame(columns=['text', 'emotion'])
-data = list(arff.load('Corpus.de.tweets.en.français/DEFT2015-arff/Train2.2.arff'))
 
-for i in range(len(data)):
-    item = data[i]
-    df.loc[i] = [item[0], item[1]]
+def loadDEFT2015():
+    df = pd.DataFrame(columns=['text', 'emotion'])
+    data = list(arff.load('Corpus.de.tweets.en.français/DEFT2015-arff/Train2.2.arff'))
 
-return df
+    for i in range(len(data)):
+        item = data[i]
+        df.loc[i] = [item[0], item[1]]
 
+    return df
 
 
 # loading txt dataset
 
-import os
-from golem.transformers import basic_transformer
-from golem.transformers import tweet_contains_tag_transformer
-from golem.parsers import csvtojson
-from golem.pipeline import pipeline
+def loadEmotionTweet(train_path, dev_path, max_features=100000, maxlen=150):
+    train = pd.read_csv(train_path,sep='\t',index_col=[0])
+    dev = pd.read_csv(dev_path,sep='\t',index_col=[0])
+    train = pd.concat([train,dev])
+    train = train.sample(frac=1)  # just shuffle
+    train['Tweet'] = train['Tweet'].apply(lambda x:x.lower())
 
-def loadEmotionTweet():
-    parser = csvtojson.CsvToJsonParser('data/data_csv/2018-E-c-En-train.txt')
+    #
+    #
+    # x_train = train['Tweet'].apply((lambda x: re.sub('[^a-zA-z0-9\s]', '', x)))
+    # y_train = train.iloc[:,1:].values
+    # print(y_train)
+    #
+    # tokenizer = text.Tokenizer(num_words=max_features)
+    # tokenizer.fit_on_texts(x_train)
+    # list_tokenized_train = tokenizer.texts_to_sequences(x_train)
+    # X_t = sequence.pad_sequences(list_tokenized_train, maxlen=maxlen)
+    # word_index = tokenizer.word_index
 
-    if not os.path.exists('data/data_json'):
-        os.makedirs('data/data_json')
+    return train
 
-    parser.export_json('data/data_json/data.json')
-    data = parser.get_pandas_data_frame()
-    return data
+
+# df = loadEmotionTweet("Corpus.de.tweets.en.anglais/EmotionTweet/2018-E-c-En-train.txt",
+#              "Corpus.de.tweets.en.anglais/EmotionTweet/2018-E-c-En-dev.txt")
+#
+# df.to_pickle("EmotionTweet.bin")
+# print(pd.read_pickle("EmotionTweet.bin"))
+# # loadDEFT2015()
